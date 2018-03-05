21709ec8b078a3366bbfc0a39116ea3450426ad1
XanderLiu <xander.liu@outlook.com>
2018-03-05 21:29:18 +0800

    项目导入

---
+++ src/main/java/RunMain.java
@@ -0,0 +1,14 @@
+public class RunMain {
+    public static void main(String[] args){
+        //<editor-fold desc="Description">
+        System.out.print("sdfsdfdsf");
+        System.out.print("sdfsdfdsf");
+        System.out.print("sdfsdfdsf");
+        System.out.print("sdfsdfdsf");
+        System.out.print("sdfsdfdsf");
+        System.out.print("sdfsdfdsf");
+        System.out.print("sdfsdfdsf");
+        //</editor-fold>
+    }
+
+}
---
+++ src/main/java/com/xander/hadoop/HDFSMain.java
@@ -0,0 +1,9 @@
+package com.xander.hadoop;
+
+/**
+ * @author xander
+ * @Description:通过Java API 操作HDFS文件系统
+ * @date 2018/3/2 15:58
+ */
+public class HDFSMain {
+}
